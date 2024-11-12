41481c51f74819e7b2b7b6354800625d02825b91
Bruno Felipe dos Santos Pereira <132142056+BrunoFelipeDevt@users.noreply.github.com>
2024-11-12 13:50:04 +0000

    feat: adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-  return <h1>Você é íncrivel!! Muito obrigado por se importar.</h1>
+function Home() {
+  return <h1>Você é íncrivel!! Muito obrigado por se importar.</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
